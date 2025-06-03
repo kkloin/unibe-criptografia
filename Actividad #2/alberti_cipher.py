a9e21e8c1fb7ac747ecbab4db383535a4ab7c2f5
raulelunap <raulelunap@gmail.com>
2025-06-02 22:53:02 -0400

    refactor: Remove File headers

--- Actividad #2/alberti_cipher.py
+++ Actividad #2/alberti_cipher.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Implementación del Cifrado de Alberti
 =====================================
--- Actividad #2/ejemplo_interactivo.py
+++ Actividad #2/ejemplo_interactivo.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Ejemplo Interactivo del Cifrado de Alberti
 ==========================================
